b6f16af447fa45a31b407eda1d54e38ed34dbf11
usiu1993 <piotrmachowski@hotmail.com>
2017-01-07 21:51:39 +0100

    final

--- datownik.c
+++ datownik.c
@@ -12,129 +12,106 @@
 #define CLOCKID CLOCK_REALTIME
 #define SIG SIGUSR1
 #define NAME 15
+#define NR 3
 #define BLOCK 31
-#define errorExit(blad) { fprintf(stderr,blad); fprintf(stderr, "nr bledu: %d\n", errno); exit(EXIT_FAILURE); } 
+#define DANE 32
+#define errorExit(blad) { fprintf(stderr,blad); fprintf(stderr, " nr bledu: %d\n", errno); exit(EXIT_FAILURE); } 
 
 
 timer_t timerid;
-sig_atomic_t volatile active = 1;
-int server_to_client[6];
+sig_atomic_t volatile aktywny = 1;
+int *kanal;
+int *pkanal;
 char** kanalFifo;
-//char* str3;
+int* kanalPipe;
 int ret;
 float odchyl=0;
 int odstep;
 int liczFifo;
-//int LICZ=0;
+int liczPipe;
 int PIPE=0;
-int pipeWe;
+int FIFO=0;
 char *wiad;
 struct itimerspec itimer;
 struct timespec czasReal;
-int errno;
-int errnum;
 
-
-//----------------------------------------------------------------------------
 
 void timer_handler (int signum)
 {
-	//static int count = 0;
-	//printf ("timer expired %d times\n", ++count);
 	struct sigaction sa1;
 	struct itimerval timer1;
 	srand( time( NULL ) );
-	/* Install timer_handler as the signal handler for SIGVTALRM.  */
+		// Konfiguracja sygnału dla timera
 	memset (&timer1, 0, sizeof (timer1));
 	memset (&sa1, 0, sizeof (sa1));
 	sa1.sa_handler = &timer_handler;
-	//sigaction (SIGVTALRM, &sa1, NULL);
+
 	if (sigaction (SIGVTALRM, &sa1, NULL) == -1)
-	{			
-		errnum = errno;
-		fprintf(stderr,"sigaction error");
-		fprintf(stderr, "Nr bledu: %d\n", errno); 
-		exit(EXIT_FAILURE);
-	}
-	//  errExit("sigaction");
+				errorExit("sigaction error");
 
-
-	float timeA = (odstep-odchyl)+(float)rand() / RAND_MAX * (2*odchyl);
-
-	while(timeA<=0)
-		timeA = (odstep-odchyl)+(float)rand() / RAND_MAX * (2*odchyl);
-	int cze2=(int)timeA;
-	timeA=timeA-cze2;
-	int cze3=timeA*1000000;
-
-
-	timer1.it_value.tv_sec = cze2;
-	timer1.it_value.tv_usec = cze3;
-
-	timer_gettime (timerid, &itimer);
-
+		//Losowy czas z podanego przedziału
+	float czasLos = (odstep-odchyl)+(float)rand() / RAND_MAX * (2*odchyl);
+	while(czasLos<=0)
+		czasLos = (odstep-odchyl)+(float)rand() / RAND_MAX * (2*odchyl);
+		//Obliczenie sekund i nanosekund	
+	int czasLosSek=(int)czasLos;
+	czasLos=czasLos-czasLosSek;
+	int czasLosNsek=czasLos*1000000;
+		//Ustawienie czasu timera
+	timer1.it_value.tv_sec = czasLosSek;
+	timer1.it_value.tv_usec = czasLosNsek;	
+		//Pobrnie czasu z zegara realnego
 	clock_gettime(CLOCK_REALTIME, &czasReal);
-
 	snprintf(wiad, 32, "sec:%ld, nsec:%ld\n",czasReal.tv_sec,czasReal.tv_nsec);
+		//Ustawienie przygotowanego timera
 	setitimer (ITIMER_VIRTUAL, &timer1, NULL);
-	if(!PIPE){
-
+			//Pisanie do podanych fifo i pipe
 		for(int i =1;i<=liczFifo;i++)
 		{
 
-			write(server_to_client[i],wiad,32);
+			write(kanal[i],wiad,DANE);
+		}
+		
+		for(int i=1;i<=liczPipe;i++)
+		{
+			
+		write(kanalPipe[i],wiad,DANE);	
+		
 		}
 
-
-	}
-	else{
-
-		write(pipeWe,wiad,32);	
-
-	}
 
 
 }
 
 
-
-static void handler(int sig, siginfo_t *si, void *uc)
+	//handler dla pomiaru czasu odstępów między stemplami
+static void handler(int sig, siginfo_t *signal, void *handler)
 {
-	if(si->si_value.sival_ptr != &timerid){
-		printf("Stray signal\n");
-	} else {
-		// printf("Caught signal %d from timer\n", sig);
-	}
-	//exit(1);
-	active = 0;
-
+	if(signal->si_value.sival_ptr != &timerid)
+		printf("Blad czasu sygnalu\n");
+	
+	aktywny = 0;
 	exit(EXIT_SUCCESS);
 }
 
 
 int main(int argc, char *argv[])
 {   
-
-
-	//str2 = malloc(50*sizeof(char*));	
 	int obowCzas=0;
 	int wyborZeg=1;
 	int wyborKan=0;
 	int opt;
 	float czas;
-	//int fd[2];
-	//pipe(fd);
-	//close(fd[0]);
 	kanalFifo = malloc(1*sizeof(char*));
+	kanalPipe = malloc(1*sizeof(int*));
 	struct sigevent sev;
 	struct itimerspec itimspec;
-	srand( time( NULL ) );
-
 	struct sigaction sa;
 	clockid_t zegar;
-
+	srand( time( NULL ) );
 	liczFifo=0;
-
+	liczPipe=0;
+	
 	if(argc<3)
 	{
 		printf("Argumenty wymagane: -m[sec] -f(fifo)/s(pipe)\n");
@@ -142,7 +119,7 @@ int main(int argc, char *argv[])
 
 	}
 
-
+		//Wybór parametrów oraz kontrola ich ilości
 	while ((opt = getopt(argc, argv, "m:d:w:c:p:f:s:")) != -1) 
 	{
 		switch (opt) 
@@ -187,18 +164,18 @@ int main(int argc, char *argv[])
 			case 'f':
 				liczFifo++;
 				wyborKan++;
-				//	gg[f] = malloc(15*sizeof(char));
 				kanalFifo[liczFifo] = malloc(NAME*sizeof(char));
 				kanalFifo[liczFifo] = optarg;
 				kanalFifo=realloc(kanalFifo,NAME*liczFifo*sizeof(char*));
+				FIFO=1;
 				break;
 			case 's':
+				liczPipe++;
 				wyborKan++;
-				pipeWe=atoi(optarg);
+				kanalPipe=realloc(kanalPipe,NR*liczPipe*sizeof(int*));
+				kanalPipe[liczPipe]=atoi(optarg);
 				PIPE=1;
 				break;
-
-
 
 			default:
 				printf("Wymagane -m[sec] -f(fifo)/s(pipe)\n");
@@ -208,62 +185,44 @@ int main(int argc, char *argv[])
 
 	}
 
-	//------------------------------------------------------------
-
 	if(!wyborKan||!obowCzas){
 		printf("nie wybrano m oraz f lub s\n");
 		exit(1);
 	}
 
-	//-----------------------------------------------	
-
-
+		// Konieczość mierzenia czasu timerem
+		// dla pomiarów zegarem lokalnym procesu
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = handler;
 	sigemptyset(&sa.sa_mask);
-	//sigaction(SIG, &sa, NULL);
 
 	if (sigaction(SIG, &sa, NULL) == -1)
-	{
 		errorExit("sigaction error\n");
-	}
+		
 
+	wiad=(char*) malloc(BLOCK*sizeof(*wiad));
+	kanal=(int*)malloc(liczFifo*sizeof(int*));
+		//Stworzenie i otwarcie plików fifo
+	for(int i=1; i<=liczFifo;i++)
+	{
+		if (access(kanalFifo[i], F_OK) == -1){
+			mkfifo(kanalFifo[i], 0666);
+		}
+		kanal[i] = open(kanalFifo[i], O_WRONLY | O_NONBLOCK);
+		if(kanal[i]==-1)
+			errorExit("blad otwarcia fifo");
+	}
+	if(!wyborZeg)
+	{
+		//Utworzenie i konfiguracja wybranego timera pomiaru czasu
 	sev.sigev_notify = SIGEV_SIGNAL;
 	sev.sigev_signo = SIG;
 	sev.sigev_value.sival_ptr = &timerid;
-	//timer_create(zegar, &sev, &timerid);
 
 	if (timer_create(zegar, &sev, &timerid) == -1)
-	{
-		errorExit("timer_create error\n");
-		//errnum = errno;
-		//fprintf(stderr,"timer_create error\n");
-		//fprintf(stderr, "Nr bledu: %d\n", errno); 
-		//exit(EXIT_FAILURE);
-	}
-
-
-
-
-	//--------------------------------------------------------------
-	wiad=(char*) malloc(BLOCK*sizeof(*wiad));
-	char **fifo = malloc(liczFifo*sizeof(char*));	
-	for(int i=1; i<=liczFifo;i++)
-	{
-		fifo[i] = malloc(NAME*sizeof(char));
-		fifo[i]=kanalFifo[i];
-		if (access(fifo[i], F_OK) == -1){
-			mkfifo(fifo[i], 0666);
-		}
-		server_to_client[i] = open(fifo[i], O_WRONLY | O_NONBLOCK);
-		if(server_to_client[i]==-1)
-		{
-			//errorExit("open error\n");
-			fprintf(stderr,"open error for %s\n",fifo[i]); 
-			fprintf(stderr, "Nr bledu: %d\n", errno);
-		}
-	}
-
+		errorExit("timer_create error\n");	
+		
+		//Obliczenie sek/nsek dla timera odliczającego czas
 	int czasSek=(int)czas;
 	czas=czas-czasSek;
 	int czasNsek= czas*1000000000;
@@ -272,63 +231,55 @@ int main(int argc, char *argv[])
 	itimspec.it_interval.tv_sec = itimspec.it_value.tv_sec;
 	itimspec.it_interval.tv_nsec = itimspec.it_value.tv_nsec;
 	timer_settime(timerid, 0, &itimspec, NULL);
-
+	}
+		// Konfiguracja sygnału dla timera
 	struct itimerval timer;
-
-	/* Install timer_handler as the signal handler for SIGVTALRM.  */
 	memset (&sa, 0, sizeof (sa));
 	sa.sa_handler = &timer_handler;
-	//sigaction (SIGVTALRM, &sa, NULL);
 
 	if (sigaction (SIGVTALRM, &sa, NULL) == -1)
-	{
 		errorExit("sigaction error\n");
-	}
+	
 
-
+		//Losowy czas z podanego przedziału
 	float czasLos=(odstep-odchyl)+(float)rand() / RAND_MAX * (2*odchyl);
 	while(czasLos<=0)
 		czasLos = (odstep-odchyl)+(float)rand() / RAND_MAX * (2*odchyl);
+		//Obliczenie sekund i nanosekund
 	int czasLosSek=(int)czasLos;
 	czasLos=czasLos-czasLosSek;
 	int czasLosNsek=czasLos*1000000;
-
-
-	/* Configure the timer to expire after 250 msec...  */
+	
+		//Ustawienie czasu pierwszego wygaśnięcia timera
 	timer.it_value.tv_sec = czasLosSek;
-	//printf("timer sec: %ld\n",timer.it_value.tv_sec);
 	timer.it_value.tv_usec = czasLosNsek;
-	/* ... and every 250 msec after that.  */
+		//Następne czasy są losowane w handlerze
 	timer.it_interval.tv_sec = 0;
 	timer.it_interval.tv_usec = 0;
-	/* Start a virtual timer.  It counts down whenever this process is
-	   executing.  */
+		//Ustawienie przygotowanego timera
 	setitimer (ITIMER_VIRTUAL, &timer, NULL);
-
 	if (setitimer (ITIMER_VIRTUAL, &timer, NULL) == -1)
-	{			errorExit("settimer error\n");
-	}	
+				errorExit("settimer error\n");
+	
+		//Praca programu
+	while (aktywny){}
 
-	while (active){
 
-	}
-
+		//Zwolnienie pamięci
 	for(int i=1;i<=liczFifo;i++)
 	{
-		close(server_to_client[i]);
-		ret = remove(fifo[i]);
+		close(kanal[i]);
+		ret = remove(kanalFifo[i]);
 		if (ret != 0)  {
-			fprintf(stderr,"\nnie usunieto %s",fifo[i]);
+			fprintf(stderr,"\nnie usunieto %s",kanalFifo[i]);
 			fprintf(stderr,"\nerrno: %d",errno);
 			exit(9);
 		}
-		unlink(fifo[i]);
-		free(fifo[i]);
+		unlink(kanalFifo[i]);
 		free(kanalFifo[i]);
-		exit(EXIT_SUCCESS);
-
 	}
-	free(fifo);
 	free(kanalFifo);
+	free(kanal);
+	free(kanalPipe);
 	free(wiad);
 }
--- powielacz.c
+++ powielacz.c
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -7,281 +6,191 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
+#define DANE 32
+#define NAZWA 20
+#define NR 3
+#define errorExit(blad) { fprintf(stderr,blad); fprintf(stderr, " nr bledu: %d\n", errno); exit(EXIT_FAILURE); } 
 
-#define DANE 31
-#define NAZWA 30
-#define errorExit(blad) { fprintf(stderr,blad); fprintf(stderr, "nr bledu: %d\n", errno); exit(EXIT_FAILURE); } 
+	//Sygnał SIGPIPE gdy zamykamy odbiorcę
+void sigpipe_handler(int signum){
 
+	fprintf(stderr,"-> sygnał SIGPIPE %d od odbiorcy <-\n",signum);
 
- void sigpipe_handler(int signum){
-
-        printf("Otrzymano sygnał SIGPIPE %d\n",signum);
-        
 }
-
-void remove_element(int *array, int index, int array_length)
-{
-   int i;
-   for(i = index; i < array_length - 1; i++) array[i] = array[i + 1];
-}
-
-
-void zamykanie(int* kolejka, int iFifo)
-{
-	
-						for(int i=0;i<iFifo;i++)
-						{
-						if(kolejka[i]==0)
-						{
-						for(int j=i;j<iFifo;j++)
-						kolejka[j]=kolejka[j+1];
-						iFifo--;
-						if(iFifo==0)
-								errorExit("wszystkie kanały nie są aktywne\n");
-						}
-						}
-								
-	
-	}
-
-
+	//Funkcja czytjąca z stdin i wysyłająca do aktywnych kanałów
 void odczytZapis(int* kolejka,int iFifo)
 {
-	
-	
 	struct sigaction sigpipeS;
 	sigpipeS.sa_handler = sigpipe_handler;
-    sigpipeS.sa_flags   = 0;    
+	sigpipeS.sa_flags   = 0;    
 
-    sigemptyset( &sigpipeS.sa_mask); 
-    sigaddset  ( &sigpipeS.sa_mask, SIGINT);
+	sigemptyset(&sigpipeS.sa_mask); 
+	sigaddset(&sigpipeS.sa_mask, SIGINT);
+	ssize_t ilBajt;   
+	char* bufor=(char*) malloc(DANE*sizeof(*bufor));
 
-	 ssize_t ilBajt;
-	 
-    char buffer[DANE];
-    for(;;) {
-
-		while(read(STDIN_FILENO, buffer, 32)){
-	
-       for(int i=0;i<iFifo;i++){
-		//int FifoRem=iFifo;
-			if(kolejka[i]!=0){
-				//nieaktywne--;
-				//printf(" w kol %d\n",nieaktywne);
-					if (sigaction(SIGPIPE, &sigpipeS, NULL) == -1)
+	for(;;) {
+			//Czytanie wyjscia datownika
+		while(read(STDIN_FILENO, bufor, DANE))
 		{
-				errorExit("sigaction error\n");
-		}
-				
-				if ((ilBajt = write(kolejka[i], buffer, DANE)) == -1){
-				//int abc;
-					   fprintf(stderr,"blad odczytu, zamkniety strumien, blad nr %d\n",errno);
-                       kolejka[i]=-1;
-					   close(kolejka[i]);
-                       //unlink(kolejka[i]);
-                       fprintf(stderr,"wyrzucam kanał /tmp/%d\n",i);
-        
-						for(int i=0;i<iFifo;i++)
-						{
-						if(kolejka[i]==-1)
-						{
-						
-							
-						for(int j=i;j<iFifo;j++)
-						kolejka[j]=kolejka[j+1];
-						iFifo--;
+			for(int i=0;i<iFifo;i++){
+
+					if (sigaction(SIGPIPE, &sigpipeS, NULL) == -1)
+						errorExit("sigaction error\n");
+						//Przypadek 1 zamknięcie strumienia 
+					if ((ilBajt = write(kolejka[i], bufor, DANE)) == -1)
+					{
+						fprintf(stderr,"Zerwano połączenie! wyrzucam kanał o deskryptorze %d\n",kolejka[i]);
 						close(kolejka[i]);
-						if(iFifo==0)
-						{	
-								//close(kolejka[k]);
-								errorExit("wszystkie kanały nie są aktywne\n");//Decreasing the length of the array
+						kolejka[i]=-1;
+						for(int i=0;i<iFifo;i++)
+						{		//Zaktualizowanie obsługiwanych kolejek
+							if(kolejka[i]==-1)
+							{
+
+								for(int j=i;j<iFifo;j++)
+									kolejka[j]=kolejka[j+1];
+								iFifo--;
+
+								if(iFifo==0)
+									errorExit("Żaden z kanałów nie jest aktywny");
+							}
+
 						}
+
+					}	//Przypadek 2 przepełnienie kolejki
+					else if(ilBajt<DANE)
+					{
+						fprintf(stderr,"Blad odczytu, przepelnione fifo\n");
+						close(kolejka[i]);
+						kolejka[i]=-1;					
+						fprintf(stderr,"wyrzucam kanał\n"); 
+						for(int i=0;i<iFifo;i++)
+						{		//Zaktualizowanie obsługiwanych kolejek
+							if(kolejka[i]==-1)
+							{
+								for(int k=i;k<iFifo;k++)
+									kolejka[k]=kolejka[k+1];
+								iFifo--;
+								if(iFifo==0)
+								{
+									errorExit("Żaden z kanałów nie jest aktywny\n");
+								}
+							}
 						}
-						
-						}
-						
-				}
-				else if(ilBajt<DANE)
-				{
-					fprintf(stderr,"blad odczytu, przepelnione fifo\n");
-					kolejka[i]=-1;
-					close(kolejka[i]);
-					fprintf(stderr,"wyrzucam kanał /tmp/%d\n",i); 
-					for(int i=0;i<iFifo;i++)
-						{
-						if(kolejka[i]==-1)
-						{
-						for(int k=i;k<iFifo;k++)
-						kolejka[k]=kolejka[k+1];
-						iFifo--;
-						if(iFifo==0)
-						{
-							
-								errorExit("wszystkie kanały nie są aktywne\n");//Decreasing the length of the array
-						}
-						}
-						}
-				
-				}
-			/*	else
-				printf("przeslano %ld bajtow\n", ilBajt);*/
-				}
-			
-			}}
-			
-			
-			 break;
-		
+					}
+			}
+		}
+
+		break;
+	}           
 }
-     
-              
-    }
 
 
 int main(int argc, char** argv)
 {	
-	
 	int DIAGNOSTIC =0;	
 	int opt;
 	int ilKolejek;
-	//wiad=(char*) malloc(BLOCK*sizeof(*wiad));
-	char* nazwa=(char*) malloc(20*sizeof(*nazwa));
-	char* nr=(char*) malloc(3*sizeof(*nr));
-	char* nazwaPliku=(char*) malloc(20*sizeof(*nazwaPliku));
+	char* nazwa=(char*) malloc(DANE*sizeof(*nazwa));
+	char* nr=(char*) malloc(NR*sizeof(*nr));
+	char* nazwaPliku=(char*) malloc(DANE*sizeof(*nazwaPliku));
 	int diag;
 
-
-	  while ((opt = getopt(argc, argv, "p:c:L:")) != -1) 
+	while ((opt = getopt(argc, argv, "p:c:L:")) != -1) 
 	{
-               switch (opt) 
-	       {
-	       case 'p':
-		 nazwa=optarg;
-                   break;
-           case 'c':
-    		ilKolejek=atoi(optarg);
-                   break;
-           case 'L':
-			DIAGNOSTIC=1;
-			nazwaPliku=optarg;
+		switch (opt) 
+		{
+			case 'p':
+				nazwa=optarg;
 				break;
-		default:
-                   printf("Wymagane: -p<string> -c<int>\n");
-                   exit(1);
-		
-			}
+			case 'c':
+				ilKolejek=atoi(optarg);
+				break;
+			case 'L':
+				DIAGNOSTIC=1;
+				nazwaPliku=optarg;
+				break;
+			default:
+				printf("Wymagane: -p<string> -c<int>\n");
+				exit(1);
+
+		}
 	}
-
-		if(DIAGNOSTIC)
+		//Jeżeli wybrano parametr -L, informacje diag. gromadzone w pliku
+	if(DIAGNOSTIC)
 	{
-	 diag = open(nazwaPliku, O_RDWR|O_CREAT|O_APPEND, 0600);
-    if (-1 == diag) { errorExit("blad otwarcia pliku\n");}	
+		diag = open(nazwaPliku, O_RDWR|O_CREAT|O_APPEND, 0600);
+		if (-1 == diag) { errorExit("blad otwarcia pliku\n");}	
 
-	if (-1 == dup2(diag, fileno(stderr))) { errorExit("blad przekierowania stderr\n");}
-}
-	
-	
-	
+		if (-1 == dup2(diag, fileno(stderr))) { errorExit("blad przekierowania stderr\n");}
+	}
 	char *nazwaKol=(char*) malloc(NAZWA*sizeof(*nazwaKol));
 	int *fd=(int*) malloc(ilKolejek*sizeof(*fd));
 	strcpy(nazwaKol, nazwa);
-
 	char **kolejki = malloc(sizeof *kolejki * ilKolejek);
-
-  for (int i = 0 ; i<ilKolejek; i++)
-  {
 	struct stat sBuf;
-    kolejki[i] = malloc(sizeof *kolejki[i] * NAZWA);
-    sprintf(nr,"%d",i);
-	strcat(nazwaKol, nr);
-
-		memset (&sBuf, 0, sizeof (sBuf));
-	stat(nazwaKol, &sBuf); 
-
-	if (S_ISFIFO(sBuf.st_mode))
-	{	//printf("w fifo %d\n",i);
-		strcpy(kolejki[i],nazwaKol);
-	}
-	else
-		fprintf(stderr,"%s nie jest plikiem fifo\n",nazwaKol);
-		    
-    strcpy(nazwaKol, nazwa);
- 
-  }
-	int *d=(int*) malloc(ilKolejek*sizeof(*d));
-	for(int j=0;j<ilKolejek;j++)
+	for (int i = 0 ; i<ilKolejek; i++)
 	{
-		
-		printf("%s\n",kolejki[j]);
+		kolejki[i] = malloc(sizeof *kolejki[i] * NAZWA);
+		sprintf(nr,"%d",i);
+		strcat(nazwaKol, nr);
+		memset (&sBuf, 0, sizeof (sBuf));
+		stat(nazwaKol, &sBuf); 
+			//sprawdzenie czy plik jest kolejką fifo
+		if (S_ISFIFO(sBuf.st_mode))
+			strcpy(kolejki[i],nazwaKol);
+		else
+			fprintf(stderr,"%s nie jest plikiem fifo\n",nazwaKol);
 
-		printf("------------\n");
-		}
-	
-	
-	
-	int ile=0;
+		strcpy(nazwaKol, nazwa);
+	}
+	int iloscAktyw=0;
 	int* aktywneKol=(int*)malloc(1*sizeof(*aktywneKol));
 	for(int k=0;k<ilKolejek;k++)
-	{	
-		fd[k]=open(kolejki[k],O_WRONLY | O_NONBLOCK);	
-//fd[k]=open(kolejki[k],O_RDWR | O_NONBLOCK);
+	{		//sprawdzenie czy plik jest otwarty do pisania
+		fd[k]=open(kolejki[k],O_WRONLY |O_NONBLOCK);	
 		if(fd[k]!=-1)
-		{ile++;
-			aktywneKol=realloc(aktywneKol,ile*sizeof(*aktywneKol));
-			//printf("%s is opened to write\n",kolejki[k]);
-			//d[k]=fd[k];	
-			aktywneKol[ile-1]=fd[k];
+		{	fprintf(stderr,"%s otwarto do pisania, deskryptor: %d\n",kolejki[k],fd[k]);
+			iloscAktyw++;
+			aktywneKol=realloc(aktywneKol,iloscAktyw*sizeof(*aktywneKol));	
+			aktywneKol[iloscAktyw-1]=fd[k];
 		}
 		else if(strcmp(kolejki[k],"")!=0){
-			//d[k]=0;
-		fprintf(stderr, "Blad otwarcia fifo %s, nr bledu %d\n",kolejki[k],errno);
-		close(fd[k]);
-	}		
+			fprintf(stderr,"%s nie jest otwarta do pisania\n",kolejki[k]);
+			close(fd[k]);
+		}	
+		
 	}
+	if(!iloscAktyw) 
+		{
+			fprintf(stderr,"Żadna kolejka nie jest otwarta do pisania\n");
+			exit(EXIT_SUCCESS);
+		}	
 	
-	
-	for(int a=0;a<ile;a++)
-		printf("dd %d\n",aktywneKol[a]);
-	
-	printf("ILE TO %d\n",ile);
-	odczytZapis(aktywneKol,ile);
-	
-	if(DIAGNOSTIC)
-{		
-	//fflush(stdout);
-	 close(diag);
-}	
+		//Wywołanie funkcji dla wyselekcjonowanych kolejek
+	odczytZapis(aktywneKol,iloscAktyw);
+
+	if(DIAGNOSTIC)		
+		close(diag);
+
 	fprintf(stderr,"\n===========================\n");
 	fprintf(stderr,"Koniec dzialania programu. \n");
 	fprintf(stderr,"===========================\n");
-	/*
-	for(int i=0;i<ile;i++)
-	{
-		close(dd[i]);
-	}
-		*/
-	//free(dd);
-	//free(fd);
+		//Zamknięcie pozostałych połączeń
+	for(int i=0;i<iloscAktyw;i++)
+		close(aktywneKol[i]);
 
-	//free(nr);
-	//free(nazwaKol);
-	//free(nazwaPliku);
-	//free(nazwa);
+		//Zwolnienie pamięci
+	for(int k=0;k<ilKolejek;k++)
+		free(kolejki[k]);
 
-	/*
-	for(int i=0;i<ilKolejek;i++)
-	{
-		int ret = remove(kolejki[i]);
-		if (ret != 0)  {
-			fprintf(stderr,"\nnie usunieto %s",kolejki[i]);
-			fprintf(stderr,"\nerrno: %d",errno);
-			exit(9);
-		}
-		unlink(kolejki[i]);
-		free(kolejki[i]);
-		
-		}
-	*/
+	free(kolejki);
+	free(fd);
+	free(nazwaKol);
+	free(aktywneKol);	
+	free(nr);
+	free(nazwaPliku);
 	return 0;
 
 }

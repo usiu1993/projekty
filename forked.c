a54869cc2692bd6fc9cf83b7c93b2eb05ddaf677
usiu1993 <piotrmachowski@hotmail.com>
2017-01-19 16:08:14 +0100

    send if signal

--- forked.c
+++ forked.c
@@ -4,98 +4,103 @@
 #include <time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+
+
+
+
+
+
+int Ppid;
+ int fd[10][2];
+char buffer1[255];
+     
+   
+    long iterator=0;
+
+void komunikat(int sig) {
+	FILE *fileptr= fopen("plik.txt", "rb");
+if(getpid()==Ppid){
+	//while(1){
+      for(int i=0; i<10; i++)
+		{	fseek(fileptr, iterator-1, SEEK_SET);
+			fread(buffer1, 1, 1, fileptr);  
+			printf("buffer to %s\n",buffer1);
+			
+			write(fd[i][1], buffer1, 1);
+			iterator++;
+		}
+
+	}
+    
+	alarm(3);    
+	signal(SIGALRM, komunikat);
+}
+
+
 
 int main(int argc, char *argv[])
 {
-    int fd[10][2];
+	int IMP=1;
+	 
+    char buffer12[255];
+    long filelen;
+//    int i;
+	
   // char* wys="tekst";
 	//int forker,forker1;
-int Ppid=getpid();
+	 Ppid=getpid();
 	printf("parent pid :%d\n",Ppid);
     // create pipe descriptors
-    for(int i =0; i<7; i++)
+    for(int i =0; i<10; i++)
 		pipe(fd[i]);
-	//int I=1;
+
 	int pid;
 	int i;
 	char wys[255]="costam";
 
-if(getpid()==Ppid){
-      for(int i=0; i<7; i++)
-			write(fd[i][1], wys, sizeof(wys));
-        printf("Parent(%d) send value: %s\n", getpid(), wys);
-	}
 
+ signal(SIGALRM, komunikat);
+	alarm(0);
 
-for(i = 0; i<6; i++) {
+if(IMP!=0)
+{
+	printf("JEST TU DO CHUJA\n");
+for(i = 0; i<9; i++) {
     pid = fork();
     if(pid < 0) {
         printf("Error");
         exit(1);
     } else if (pid == 0) {
         printf("Child (%d): %d\n", i + 1, getpid());
-        //exit(0);
         break; 
     } else  {
-       wait(NULL);
+       //wait(NULL);
     }
 }
-//printf("tu dziecko o %d \n",getpid());
+IMP=0;
+}
 	
-	
+	while(1){
 	if(getpid()!=Ppid)
 	{
 		
 		int diff=getpid()-Ppid;
-		printf("tu dziecko o %d a roznica to %d\n",getpid(),diff);
+		//printf("tu dziecko o %d a roznica to %d\n",getpid(),diff);
 	
 	
 	
         close(fd[diff][1]);
-        char ot[3];
+        char ot[255];
         // now read the data (will block)
-        read(fd[diff][0], ot,sizeof(ot) );
+        read(fd[diff][0], ot,1);
         printf("Child(%d) received value: %s\n", getpid(), ot);
+        memset(ot,0,1);
+	}
   }
-   /*
-   for (int i = 0; i < 1; ++i) {
-  
-    
-    forker = fork();
-    printf("jestem tu %d\n",i);
-   
-    if (forker && I!=0) {
-		p=getpid();
-		int differ=p-Ppid;
-		close(fd[differ][0]);
-//        val = 100;
-        printf("jestem tu\n");
-  
-
-        // close the write descriptor
-        close(fd[differ][1]);
-
-		I=0;
-        continue;
-    } else if (forker == 0 ) {
-		
-		
-        break;
-    } 
-    else {
-        printf("fork error\n");
-        exit(1);
-    }
-}
-   
-
-forker1=getpid();
 
 
-        // close the read-descriptor
-        close(fd[diff][0]);
-    
-  
-*/    
+ 
+ //sleep(12);
     return 0;
 }
